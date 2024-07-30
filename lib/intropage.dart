2cef281a8759e28c1e1fce793af09c73a2424e7e
Hastik <hastikmangukiya07@gmail.com>
2024-07-31 00:04:09 +0530

    change file name

--- lib/intropage.dart
+++ lib/intropage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class Intropage extends StatelessWidget {
+  const Intropage({super.key});
 
   @override
   Widget build(BuildContext context) {
