669598fb9379cdc7326d609ff4ca07c0d9fa8789
Hastik <hastikmangukiya07@gmail.com>
2024-08-03 12:08:56 +0530

    create Search Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:blog_app/homepage.dart';
+import 'package:blog_app/searchpage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage()
+      home: const SearchPage()
     );
   }
 }
---
+++ lib/searchpage.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Container(
+        color: Colors.grey.shade500,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+            backgroundColor: Colors.grey.shade500,
+            color: Colors.black,
+            activeColor: Colors.black,
+            tabBackgroundColor: Colors.yellow.shade800,
+            padding: const EdgeInsets.all(16),
+            tabs: const [
+              GButton(icon: Icons.home),
+              GButton(icon: Icons.search),
+              GButton(icon: Icons.add_circle),
+              GButton(icon: Icons.notifications),
+              GButton(icon: Icons.person),
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Container(
+                    width: 350,
+                    height: 49,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: const Row(
+                      children: [
+                        SizedBox(width: 15,),
+                        Icon(Icons.arrow_back_rounded),
+                        SizedBox(width: 15,),
+                        Text('Search by title...')
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                width: 350,
+                height: 115,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Blog', style: TextStyle(fontWeight: FontWeight.bold, height: 10)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
